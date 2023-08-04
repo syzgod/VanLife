55ac4bdffe5603046af50e4713db1ad9b35e1108
syzgod <viktor.berczeli@gmail.com>
2023-08-04 13:45:33 +0200

    Add Vans page and mock server

--- index.jsx
+++ index.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Vans from './pages/Vans';
+import About from './pages/About';
+
+import './server.js';
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <Link className='site-logo' to='/'>
+          #VanLife
+        </Link>
         <nav>
-          <Link to="/about">About</Link>
+          <Link to='/vans'>Vans</Link>
+          <Link to='/about'>About</Link>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ pages/Vans.jsx
@@ -0,0 +1,5 @@
+const Vans = () => {
+  return <h1>Vans page goes here 🚐</h1>;
+};
+
+export default Vans;
