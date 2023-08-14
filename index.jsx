00259f395bb8cdf20e741e9409d8f46ebc34908c
syzgod <viktor.berczeli@gmail.com>
2023-08-14 13:19:34 +0200

    Fetch Data and add Van Details page

--- index.jsx
+++ index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import Vans from './pages/Vans';
 import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
 
-import './server.js';
+import './server';
 
 function App() {
   return (
@@ -15,14 +16,15 @@ function App() {
           #VanLife
         </Link>
         <nav>
-          <Link to='/vans'>Vans</Link>
           <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/vans' element={<Vans />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
